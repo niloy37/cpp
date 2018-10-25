215a6f3dcaf9f2c393c5873c547b233d43f8590b
Niloy Rahman <havocisgod@gmail.com>
2018-10-25 23:04:45 +0600

    Add files via upload

---
+++ AveragSum.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+int main () {
+
+cout<<"Enter three numbers"<<endl;
+int x;
+int y;
+int z;
+
+cin>>x;
+cin>>y;
+cin>>z;
+
+double average = ((x+y+z)*1.0)/3;
+int  sum = (x+y+z);
+cout<<"The sum is "<<sum<<endl;
+cout<<"The average is "<<average<<endl;
+
+
+
+return 0;
+}
