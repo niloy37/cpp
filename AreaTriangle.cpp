7c4573640f451ce18c71ee116ad39e72c52ad6ce
Niloy Rahman <havocisgod@gmail.com>
2018-10-27 01:17:11 +0600

    area , taking input from user
    area of a triangle taking input from user. 
    formula area = 1/2* base* height;

---
+++ AreaTriangle.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+
+cout<<"Enter the base and height of a triangle " <<endl;
+int x ;
+int y;
+cin>>x;cin>>y;
+
+double area = 0.5*x*y;
+
+cout<<"The area of the triangle is ="<<area<<endl;
+return 0;
+
+
+
+
+
+
+}
