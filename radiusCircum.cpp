0e4945ee35937629154acb9c90ed0ed111ab8b8c
Niloy Rahman <havocisgod@gmail.com>
2018-10-27 01:27:06 +0600

    Add files via upload

---
+++ radiusCircum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main () {
+cout<<"Enter radius of a triangle "<<endl;
+int x;
+cin>>x;
+
+double area = 3.1416*x*x;
+double circumference = 2*3.1416*x;
+
+cout<<"Area is = "<<area<<",Circumference = "<<circumference<<endl;
+
+
+
+
+return 0;
+}
