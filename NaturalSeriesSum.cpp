44ee289ed4332f6dc4ed8163f598d063ca5bfbcd
Niloy Rahman <havocisgod@gmail.com>
2018-10-27 09:17:58 +0600

    Add files via upload

---
+++ NaturalSeriesSum.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main () {
+
+cout<<"Enter N"<<endl;
+int x;
+cin>>x;
+
+
+int sum=0;
+for(int i=1;i<=x;i++)
+    sum+=i;
+
+cout<<sum;
+
+return 0;
+}
