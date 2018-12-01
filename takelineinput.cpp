d6bd2d863ebc54d36116b8575fe85260b4173a71
Niloy Rahman <havocisgod@gmail.com>
2018-12-02 01:36:51 +0600

    Create takelineinput.cpp

---
+++ takelineinput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	cout<<"Enter a line input"<<endl;
+	string s;
+	getline(cin,s);
+	cout<<s;
+	
+	
+	return 0;
+}
