1b93cc78baa77e831f41dbe3d2b9449c9b7f3eb3
Niloy Rahman <havocisgod@gmail.com>
2018-12-03 01:11:32 +0600

    Create nameroll.cpp

---
+++ nameroll.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+ cout<<"Enter a name "<<endl;
+ string s;
+ getline(cin,s);
+
+ cout<<"Enter roll number"<<endl;
+ int x;
+ cin>>x;
+
+ cout<<"Printing name and roll number"<<endl;
+
+
+ cout<<s<<endl;
+ cout<<x;
+
+
+}
