f854e7d685ccb09e56f69fa3b3680fd0870d0a0a
Niloy Rahman <havocisgod@gmail.com>
2018-12-05 23:53:57 +0600

    Create secondlargest C.cpp

---
+++ vjudge.net/NSUPS BOOTCAMP/secondlargest C.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main () {
+int n;
+cin>>n;
+
+int a[n];
+int x,y,z;
+for(int i=0;i<n;i++){
+cin>>x>>y>>z;
+ if(x>=z&&z>=y || y>= z&& z>=x)
+    a[i]=z;
+
+    if(z>=x&&x>=y || y>=x&& x>=z)
+        a[i]=x;
+
+    if(x>=y&&y>=z || z>=y&&y>=x)
+        a[i]=y;
+
+ }
+
+for(int i=0;i<n;i++){
+    cout<<a[i]<<endl;
+}
+}
