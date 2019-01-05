4f31a603d69bca290869d1896f9171b84ca18b60
Niloy Rahman <havocisgod@gmail.com>
2019-01-05 07:04:38 -0800

    Create vector sort.cpp

---
+++ vjudge.net/NSUPS BOOTCAMP/contest3/vector sort.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+
+vector<int> v;
+
+int n;
+cin>>n;
+int x;
+
+for(int i=0;i<n;i++){
+        cin>>x;
+        v.push_back(x);
+}
+
+sort(v.begin(),v.end());
+
+for(int i=0;i<n;i++){
+
+    cout<<v.at(i)<<' ';
+}
+
+}
+
+
