9f67aa2556301edd791fc23bf9d276205b49d774
Niloy Rahman <havocisgod@gmail.com>
2019-01-12 21:25:44 +0600

    Create conditional statements.cpp

---
+++ hackerrank practice/conditional statements.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    // Write Your Code Here
+switch(n){
+    case 1: cout<<"one";break;
+    case 2: cout<<"two";break;
+    case 3: cout<<"three";break;
+    case 4: cout<<"four";break;
+    case 5: cout<<"five";break;
+    case 6: cout<<"six";break;
+    case 7: cout<<"seven";break;
+    case 8: cout<<"eight";break;
+    case 9: cout<<"nine";break;
+    default: cout<<"Greater than 9";
+}
+    return 0;
+}
