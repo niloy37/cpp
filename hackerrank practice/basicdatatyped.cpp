5bbecd7fea2b11d9882c66d4eb09dcb79599b04f
Niloy Rahman <havocisgod@gmail.com>
2019-01-12 21:21:37 +0600

    Rename inputoutput.cpp to basicdatatyped.cpp

---
+++ hackerrank practice/basicdatatyped.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdio>
+#include<iomanip>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long long int b;
+    char c;
+    float d;
+    double e;
+    //inputs
+    cin>>a>>b>>c>>d>>e;
+    //outputs
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<fixed<<setprecision(3)<<d<<endl;
+    cout<<fixed<<setprecision(9)<<e;
+    return 0;
+}
+
