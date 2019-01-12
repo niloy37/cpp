16c93a34820588c27b57db496cb641255b0828e4
Niloy Rahman <havocisgod@gmail.com>
2019-01-12 21:43:10 +0600

    Create functions(max_of_four).cpp

---
+++ hackerrank practice/functions(max_of_four).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a,int b,int c,int d){
+int x;
+x=max(a,b);
+x = max(c,x);
+x= max(d,x);
+return x;
+
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+
