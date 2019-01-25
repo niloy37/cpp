e67665f439f8a97aaaee9741b4729fc91f8ebc5b
Niloy Rahman <havocisgod@gmail.com>
2019-01-25 12:29:58 +0600

    Create beautiful year.cpp

---
+++ vjudge.net/NSUPS BOOTCAMP/contest 4/beautiful year.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+int n;
+cin>>n;
+n++;
+while(true){
+
+    int a= n/1000;
+    int b = n/100%10;
+    int c = n/10%10;
+    int d = n%10;
+
+    if(a!=b && a!=c && a!=d && b!=c && b!=d && c!=d){
+        break;
+    }
+
+
+n++;
+
+}
+cout<<n<<endl;
+
+
+
+
+
+}
