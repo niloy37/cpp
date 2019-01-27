e2ba51758bb67c4cd8c8145932ed9b018588122d
Niloy Rahman <havocisgod@gmail.com>
2019-01-27 21:44:39 +0600

    Create to carry or not to carry.cpp

---
+++ vjudge.net/NSUPS BOOTCAMP/contest 4/to carry or not to carry.cpp
@@ -0,0 +1,15 @@
+/**
+   Bismillah-ar-Rahman-ar Rahim
+
+   Niloy Rahman
+   B.Sc in Computer Science and Engineering.
+   North South University,Dhaka.
+
+**/
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+int a,b;
+while(cin>>a>>b)
+cout<<(a^b)<<endl;
+}
