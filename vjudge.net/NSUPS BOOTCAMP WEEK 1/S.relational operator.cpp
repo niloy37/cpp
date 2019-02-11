870e8ffd41ebc958137855f38f049cba9d1879ea
Niloy Rahman <havocisgod@gmail.com>
2019-02-11 18:56:27 +0600

    Create S.relational operator.cpp

---
+++ vjudge.net/NSUPS BOOTCAMP WEEK 1/S.relational operator.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+int n;
+cin>>n;
+int a,b;
+while(n--){
+
+    cin>>a>>b;
+    if(a>b)cout<<">"<<endl;
+    else if(a<b)cout<<"<"<<endl;
+    else cout<<"="<<endl;
+    }
+}
