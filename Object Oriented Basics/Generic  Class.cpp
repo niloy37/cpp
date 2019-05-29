72c0d456492f00c76a9090568843f054c776b42d
Niloy Rahman <havocisgod@gmail.com>
2019-05-29 00:11:36 -0700

    Create Generic  Class.cpp

---
+++ Object Oriented Basics/Generic  Class.cpp
@@ -0,0 +1,20 @@
+//g++  5.4.0
+
+#include <iostream>
+using namespace std;
+
+template<class T>
+class calculator {
+T num1,num2;
+    public :
+    T add(T n1,T n2){
+     return n1+n2;   
+    }
+};
+
+int main()
+{
+    calculator<float>c1;
+    cout<<c1.add(4.2,2.1);
+    //cout << "Hello, world!\n";
+}
