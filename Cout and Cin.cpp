ba56eb2a05e6975cafd9fbbef2f3f5524e93c016
Niloy Rahman <havocisgod@gmail.com>
2019-05-29 01:58:23 -0700

    Create Cout and Cin.cpp

---
+++ Cout and Cin.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter a number!" << endl;
+    int n;
+    cin>>n;
+    cout<<"You entered :"<<n<<endl;
+
+    return 0;
+}
