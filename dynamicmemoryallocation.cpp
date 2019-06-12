4e659f0f5c450ab7a39ce62f2d861c2d47e22c3d
Niloy Rahman <havocisgod@gmail.com>
2019-06-12 00:46:11 -0700

    Create dynamicmemoryallocation.cpp

---
+++ dynamicmemoryallocation.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int *px = new int;
+float *qx ;
+qx = new float;
+*qx = 1000.50;
+*px = 1000;
+cout<<px<<" "<<qx<<" "<<*px<<" "<<*qx; 
+}
