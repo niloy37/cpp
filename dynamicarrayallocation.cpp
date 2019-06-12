3f455e42df63908768bebb8cfebf9fea8c28b81e
Niloy Rahman <havocisgod@gmail.com>
2019-06-12 01:15:08 -0700

    Update dynamicarrayallocation.cpp

--- dynamicarrayallocation.cpp
+++ dynamicarrayallocation.cpp
@@ -6,7 +6,8 @@ int main() {
 	int *dp = new int [5]; //dynamic array allocation
 	int data[5]; //statical array allocation
 	cout<<data<<endl; // printing address of data0 [starting block]
-	dp[3]= 1; //setting the dynamic array 3 to 1
+	*(dp+3)= 1; //setting the dynamic array 3 to 1
 	cout<<dp[3]; //printing 
+	delete [] dp;
 	return 0;
 }
