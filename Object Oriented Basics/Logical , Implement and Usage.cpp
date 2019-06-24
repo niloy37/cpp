c7bf25bb71724bdc6425fdf4c1f4a8715c4ec8df
Niloy Rahman <havocisgod@gmail.com>
2019-06-24 00:44:31 -0700

    Update Logical , Implement and Usage.cpp

--- Object Oriented Basics/Logical , Implement and Usage.cpp
+++ Object Oriented Basics/Logical , Implement and Usage.cpp
@@ -48,6 +48,34 @@ if(length == MAX_ITEMS)
 	 else 
 		 return false;
  }
+int UnsortedType::GetLength() {
+	return length;
+}
+void UnsortedType::ResetList(){
+		
+}
+ItemType GetItem (ItemType item , bool &found){
+bool moreToSearch;
+int location = 0;
+found = false;
+	moreToSearch = (location < length );
+	while(moreToSearch && !found){
+	switch(item.comparedTo ( info [location])){
+		case LESS :
+		case GREATER : location++;moreToSearch = (location < length); break;
+		case EQUAL : found = true;
+			item = info[location];
+			break;
+	}
+	}
+	return item;
+}
+
+ItemType UnsortedType::GetNextItem(){
+	
+}
+
+
 
 
 
