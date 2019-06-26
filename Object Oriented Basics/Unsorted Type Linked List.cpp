762ec1aee04fe20cfdd8cf6490e173458cc39c91
Niloy Rahman <havocisgod@gmail.com>
2019-06-26 00:22:40 -0700

    Create Unsorted Type Linked List.cpp

---
+++ Object Oriented Basics/Unsorted Type Linked List.cpp
@@ -0,0 +1,20 @@
+#include "ItemType.h"
+class UnsortedType {
+int length ;
+ ItemType info[MAX_ITEMS];
+ int currentPos;
+ 
+ public : 
+ UnsortedType();
+ //REST 8 METHODS and addition of Destructor
+ 
+ ~UnsortedType();
+ 
+ };
+ 
+ 
+ int main () {
+ 
+ 
+ }
+ 
