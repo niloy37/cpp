9c3e407dedcec95eeb1cd6080e2248b024a50adc
Niloy Rahman <havocisgod@gmail.com>
2019-06-29 22:58:28 +0600

    Create ifndef , endif[Conditional Compilation Macros].cpp

---
+++ ifndef , endif[Conditional Compilation Macros].cpp
@@ -0,0 +1,27 @@
+/*
+The Macros include
+#ifndef, #ifdef
+#if #elseif #else
+#endif
+#endif
+
+*/
+
+#define NILOY 1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main (){
+#ifdef NILOY
+
+cout<<"Welcome"<<endl;
+#else
+cout<<"Invalid Identity";
+#endif // NILOY
+
+cout<<"Executed"<<endl;
+
+}
+
