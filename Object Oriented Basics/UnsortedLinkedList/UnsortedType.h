5421afd56fb48571eb0516c107b500fa2e505899
Niloy Rahman <havocisgod@gmail.com>
2019-07-01 00:23:19 -0700

    Create UnsortedType.h

---
+++ Object Oriented Basics/UnsortedLinkedList/UnsortedType.h
@@ -0,0 +1,14 @@
+class UnsortedType{
+NodeType *ListData;
+int length;
+NodeType *currentpos;
+
+
+public :
+UnsortedType();
+~UnsortedType();
+void putItem(ItemType item);
+void DeleteItem(ItemType item);
+void makeEmpty();
+
+}
