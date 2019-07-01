1d2bf12b7d24957f66fc79ba7f79a1aa2b6aaa10
Niloy Rahman <havocisgod@gmail.com>
2019-07-01 01:28:12 -0700

    Update UnsortedType.cpp

--- Object Oriented Basics/UnsortedLinkedList/UnsortedType.cpp
+++ Object Oriented Basics/UnsortedLinkedList/UnsortedType.cpp
@@ -10,4 +10,8 @@ listData = NULL;
 UnsotedType:: ~UnsortedType();
 void UnsortedType::putItem(ItemType item){
  NodeType *location = new NodeType;
+ location->info = item;
+ location->next = listData;
+ listData = location;
+ length++;
 }
