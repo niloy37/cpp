4efeecbba071453c9349a84cfc250083b35f430a
Niloy Rahman <havocisgod@gmail.com>
2019-07-02 22:20:24 +0600

    Create Algorithms of O(n*n) with Function.cpp

---
+++ Sorting Algorithms/Algorithms of O(n*n) with Function.cpp
@@ -0,0 +1,62 @@
+//not fully updated
+#include<bits/stdc++.h>
+#define n 7
+using namespace std;
+
+void bubbleSort(int arr []){
+	//int n = sizeof(arr)/sizeof(arr[0]);
+//Bubble Sorting Complexity O(n*n) Author ~ Havoc , Contribution MMR4 , ECE , NSU
+	for(int i=1 ; i < n ; i ++){
+		for(int j = 0 ; j < (n-i) ; j++){
+			if(arr[j] > arr[j+1]){
+				int temp = arr[j];
+				arr[j]= arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+
+}
+void SelectionSort(int arr []){
+	// Selection Sort , Complexity O(N*N) Author ~ Havoc , Contribution MMR4 , ECE , NSU
+//int n =  sizeof(arr)/sizeof(arr[0]);
+
+for(int i=0;i<n-1;i++){
+		int minIndex = i;
+		for(int j = i+1 ; j< n ; j++){
+			if(arr[j]<arr[minIndex]){
+				minIndex = j;
+			}
+
+		}
+		if(i!= minIndex){
+			int temp = arr[i];
+			arr[i] = arr[minIndex];
+			arr[minIndex] = temp;
+		}
+	}
+
+}
+
+void insertionSort(int [] arr){
+
+	
+}
+
+
+int main () {
+int arr [] { 1,3,2,5,4,7,6};
+SelectionSort(arr);
+for(int a : arr){
+	cout<<a<<" ";
+}
+cout<<endl<<endl;
+int arr2 [] {2,4,3,1,6,5,7};
+bubbleSort(arr2);
+for(int a: arr2){
+	cout<<a<<" ";
+} 
+cout<<endl<<endl;
+
+
+}
