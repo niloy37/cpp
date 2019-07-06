ca5df91ba0cea8f47ee5e8cf52aea5f6ea7704aa
Niloy Rahman <havocisgod@gmail.com>
2019-07-07 01:50:46 +0600

    Create triangleIsPossible?.cpp

---
+++ triangleIsPossible?.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main () {
+	int a,b,c;
+	cout<<"Enter three sides of the triangle"<<endl;
+	cin>>a>>b>>c;
+	if(a>=1 && b>=0 && c>=0){
+		if(a+b>c && b+c>a && c+a>b) cout<<"Triangle can be constructed"<<endl;
+		else cout<<"Triangle cannot be constructed"<<endl;
+
+	}
+	else  cout<<"Triangle cannot be constructed with the given sides(Length cannot be negative)"<<endl;
+	
+
+}
