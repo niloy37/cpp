0b28b629a34b8e272f0429c72f8979e7e17a2a7d
Niloy Rahman <havocisgod@gmail.com>
2019-07-08 01:23:09 +0600

    Update Basicmap.cpp

--- STL/Maps/Basicmap.cpp
+++ STL/Maps/Basicmap.cpp
@@ -8,5 +8,11 @@ student[2] = "StudentTwo";
 student[3] = "StudentThree";
 student[4] = "StudentFour";
 student[5] = "StudentFive";
+  /*
+  No duplicate keys , as for example 
+  student[4] = "StudentFour Two";
+  will output StudentFourTwo instead of StudentFour.
+  Last set value will be displayed
+  */
 cout<<student[4]<<endl;
 }
