f6248cb2973eb9046498f6fc113e8ed95db6d853
Niloy Rahman <havocisgod@gmail.com>
2019-07-15 00:34:01 -0700

    Update main.cpp

--- Object Oriented Basics/Sorted Type Array/main.cpp
+++ Object Oriented Basics/Sorted Type Array/main.cpp
@@ -1,6 +1,16 @@
-#include "unsorted.h"
+#include "Sorted.h"
+#include "ItemType.h"
 using namespace std;
 int main () {
-
+ItemType i1,i21,i3,i4;
+  i1.initialize(1);
+  i2.initialize(2);
+  i3.initialize(3);
+  i4.initialize(4);
+  
+  SortedType list1;
+  list1.putItem(i1);
+  list1.putItem(i4);
+  list1.putItem(i2);
 
 }
