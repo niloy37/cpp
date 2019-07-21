cfaa66e6adb9b50521921014e9ea4b0147fca4be
Niloy Rahman <havocisgod@gmail.com>
2019-07-21 23:23:53 +0600

    Create Template_3.cpp

---
+++ Template/Template_3.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define s(n)                        scanf("%d",&n)
+#define s2(a,b)				    	 scanf("%d%d",&a,&b)
+#define s3(a,b,c)					 scanf("%d%d%d",&a,&b,&c)
+#define sc(n)                       scanf("%c",&n)
+#define sl(n)                       scanf("%I64d",&n)
+#define sf(n)                       scanf("%lf",&n)
+#define ss(n)                       scanf("%s",n)
+#define forall(i,a,b)               for(int i=a;i<b;i++)
+#define foreach(v, c)               for( typeof( (c).begin()) v = (c).begin();  v != (c).end(); ++v)
+#define all(a)                      a.begin(), a.end()
+#define WHILE(n)					while(n--)
+#define pb                          push_back
+#define minimum(a)					*min_element(a.begin(), a.end())
+#define maximum(a)					*max_element(a.begin(), a.end())
+typedef long long ll;
+typedef long double ld;
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+ll power(ll a, ll n) {ll p = 1;while (n > 0) {if(n%2) {p = p * a;} n >>= 1; a *= a;} return p;}
+ll power(ll a, ll n, ll mod) {ll p = 1;while (n > 0) {if(n%2) {p = p * a; p %= mod;} n >>= 1; a *= a; a %= mod;} return p % mod;}
+
+int main () {
+
+
+return 0;
+}
