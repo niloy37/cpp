2e11ef0bc10678f1f5f7e9ac6e0b8ce7ff6d3fc9
Niloy Rahman <havocisgod@gmail.com>
2019-07-23 02:11:42 +0600

    Create argc,argv.cpp

---
+++ argc,argv.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc , char **argv){ //argc refers to the number of arguments passed to main ,
+cout<<"Hello,World";//and argv is an array of strings of the arguments
+}//atleast one argument will be passed to the main (the name of the program)
