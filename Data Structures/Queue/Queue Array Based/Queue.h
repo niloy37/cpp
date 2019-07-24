0d41816f84ea84ecec9cac05408801b68341b061
Niloy Rahman <havocisgod@gmail.com>
2019-07-24 01:21:47 -0700

    Update Queue.h

--- Data Structures/Queue/Queue Array Based/Queue.h
+++ Data Structures/Queue/Queue Array Based/Queue.h
@@ -1,1 +1,13 @@
+const int MAX_SIZE = 10;
 
+class Queue {
+  private :
+  ItemType items[MAX_SIZE];
+  int front;
+  int rear;
+  public:
+  Queue();
+  Enqueue(ItemType item);
+  ItemType Dequeue();
+  
+};
