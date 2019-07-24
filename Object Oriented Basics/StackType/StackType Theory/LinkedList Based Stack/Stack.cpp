11bbd9b263fafc6d4166ab7b35f8828f6f8c8a3b
Niloy Rahman <havocisgod@gmail.com>
2019-07-24 00:41:48 -0700

    Create Stack.cpp

---
+++ Object Oriented Basics/StackType/StackType Theory/LinkedList Based Stack/Stack.cpp
@@ -0,0 +1,21 @@
+#include "Stack.cpp"
+
+Stack::Stack(){
+topPtr = NULL;
+}
+
+void Stack::push(ItemType item){
+if(isFull()){
+throw FullStack();
+}
+else{
+NodeType *location = new NodeType;
+location->info = item; 
+location->next = topPtr;
+topPtr = location;
+}
+}
+
+void Stack::pop() {
+
+}
