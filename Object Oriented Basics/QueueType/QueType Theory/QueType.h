5418ac7491419681b3001ff374799fe8935598cb
Niloy Rahman <havocisgod@gmail.com>
2019-07-29 01:05:53 -0700

    Update QueType.h

--- Object Oriented Basics/QueueType/QueType Theory/QueType.h
+++ Object Oriented Basics/QueueType/QueType Theory/QueType.h
@@ -1,1 +1,28 @@
 
+class QueType {
+ ItemType *items;
+  int front ;
+  int rear;
+  int maxQue;
+  
+  public :
+  QueType(){
+    maxQue =501;
+    front = maxQue-1;
+    rear = maxQue-1;
+   items = new ItemType[maxQue]; 
+  }
+  QueType(int max){
+    maxQue=max+1;
+    front = maxQue-1;
+    rear = maxQue-1;
+    items = new ItemType[maxQue];
+  }
+  ~QueType();
+  
+  
+  
+  
+  
+  
+}
