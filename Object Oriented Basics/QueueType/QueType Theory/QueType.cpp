09d6387b4d4ba0b011721e4232dc41d670dbf2d4
Niloy Rahman <havocisgod@gmail.com>
2019-07-29 01:08:41 -0700

    Update QueType.cpp

--- Object Oriented Basics/QueueType/QueType Theory/QueType.cpp
+++ Object Oriented Basics/QueueType/QueType Theory/QueType.cpp
@@ -1,1 +1,10 @@
 //constructor and parameter constructor inside header
+
+bool QueType::isEmpty(){
+ if(front == rear) return true;
+  else return false;
+  }
+bool QueType::makeEmpty(){
+  front = maxQue-1;
+  rear = maxQue-1;
+}
