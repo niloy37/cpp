198c004d7598db0b4491585649a13175e753801a
Niloy Rahman <havocisgod@gmail.com>
2019-07-29 01:18:59 -0700

    Update QueType.cpp

--- Object Oriented Basics/QueueType/QueType Theory/QueType.cpp
+++ Object Oriented Basics/QueueType/QueType Theory/QueType.cpp
@@ -8,3 +8,24 @@ bool QueType::makeEmpty(){
   front = maxQue-1;
   rear = maxQue-1;
 }
+
+QueType::~QueType(){
+ delete [] items;
+ }
+void QueType::Enqueue(ItemType item){
+ if(isFull) 
+  throw fullstack();
+  else {
+  rear = (rear+1)% maxQue;
+  items[rear] = item;
+  }
+}
+void QueType::DeQueue(ItemType& item){
+ if(isEmpty())
+  throw EmptyStack();
+ else {
+ front = (front+1) %maxQue;
+ item = info[front];
+ }
+}
+
